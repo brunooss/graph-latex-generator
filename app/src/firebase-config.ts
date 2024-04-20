dfdff43f69def37a2f7c5ddd26f6ac8a8370cef4
Bruno Oliveira <bruno.oss@outlook.com>
2024-04-20 17:15:59 -0300

    feat: firebase setup

---
+++ app/src/firebase-config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnFdvA2PixeOhE4kCDvt4Uu25wh4bkXfw",
+  authDomain: "graph-latex-generator.firebaseapp.com",
+  projectId: "graph-latex-generator",
+  storageBucket: "graph-latex-generator.appspot.com",
+  messagingSenderId: "163843581862",
+  appId: "1:163843581862:web:a8815100ca07a5c4cc5167",
+  measurementId: "G-0S9RECX3HX",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
